8f11cecd64acbe5daea00c167cd203216f3fb93d
Uddhav Dahatonde <dahatondeuddhava@gmail.com>
2022-09-16 13:46:13 +0530

    Add project files.

---
+++ OverLoading.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class OverLoading
+    {
+       public int Addition(int num1,int num2)
+        {
+            return num1 + num2;
+        }
+        public int Addition(int num1,int num2,int num3)
+        {
+            return (num1 + num2 + num3);
+        }
+        public double Addition(int num1,double num2)
+        {
+            return num1 + num2;
+        }
+        public double Addition(double num1,double num2)
+        {
+            return num1 + num2;
+        }
+
+
+    }
+    public class Calculation
+    {
+        static void Main(string[] args)
+        {
+            OverLoading C1=new OverLoading();
+            Console.WriteLine(C1.Addition(10,20));
+            Console.WriteLine(C1.Addition(20,30.5));
+            Console.WriteLine(C1.Addition(10,30,40));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+    //public class Auto
+    //{
+       
+    //    public string Name { get; set; }
+    //    public int Id { get; set; }
+    //    public int Mark { get; set; }
+
+    //    public override string ToString()
+    //    {
+    //        return $"Name->{Name}\nId->{Id}\nMark->{Mark}";
+    //    }
+    //}
+    //internal class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Auto auto = new Auto();
+    //        auto.Name = "Uddhav Dahatonde";
+    //        auto.Id = 123;
+    //        auto.Mark = 95;
+    //        Console.WriteLine($"{auto }");
+    //    }
+    //}
+}
